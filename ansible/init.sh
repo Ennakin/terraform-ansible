91efbc32b7cbd5b10bd9cae3789e2f333b2c304c
Evgenii Hodkevich <ehodkevich@hr-link.ru>
2024-04-02 00:09:58 +0300

    fix

---
+++ ansible/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+script=$(realpath -s "$0")
+scriptpath=$(dirname "$script")
+
+cp -u -p $scriptpath/envs/default/env_ansible_all.env.default $scriptpath/envs/prod/env_ansible_all.env
+cp -u -p $scriptpath/envs/default/env_ansible_common.env.default $scriptpath/envs/prod/env_ansible_common.env
+cp -u -p $scriptpath/envs/default/env_ansible_dev.env.default $scriptpath/envs/prod/env_ansible_dev.env
+cp -u -p $scriptpath/envs/default/env_ansible_test.env.default $scriptpath/envs/prod/env_ansible_test.env
+cp -u -p $scriptpath/envs/default/env_ansible_staging.env.default $scriptpath/envs/prod/env_ansible_staging.env
+
+cp -u -p $scriptpath/configs/cloud-config.default $scriptpath/configs/cloud-config
--- terraform/init.sh
+++ terraform/init.sh
@@ -3,9 +3,10 @@
 script=$(realpath -s "$0")
 scriptpath=$(dirname "$script")
 
-cp -u -p $scriptpath/envs/default/all.env.default $scriptpath/envs/prod/all.env
-cp -u -p $scriptpath/envs/default/common.env.default $scriptpath/envs/prod/common.env
-cp -u -p $scriptpath/envs/default/staging.env.default $scriptpath/envs/prod/staging.env
-cp -u -p $scriptpath/envs/default/test.env.default $scriptpath/envs/prod/test.env
+cp -u -p $scriptpath/envs/default/env_tf_all.env.default $scriptpath/envs/prod/env_tf_all.env
+cp -u -p $scriptpath/envs/default/env_tf_common.env.default $scriptpath/envs/prod/env_tf_common.env
+cp -u -p $scriptpath/envs/default/env_tf_dev.env.default $scriptpath/envs/prod/env_tf_dev.env
+cp -u -p $scriptpath/envs/default/env_tf_test.env.default $scriptpath/envs/prod/env_tf_test.env
+cp -u -p $scriptpath/envs/default/env_tf_staging.env.default $scriptpath/envs/prod/env_tf_staging.env
 
 cp -u -p $scriptpath/configs/cloud-config.default $scriptpath/configs/cloud-config
