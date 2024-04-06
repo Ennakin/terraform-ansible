666d5af0d6c4b239050d47e503b556980356e163
Evgenii Hodkevich <ehodkevich@hr-link.ru>
2024-04-06 14:32:32 +0300

    fs из рабочей директории

--- terraform/environments/common/compute/main.tf
+++ terraform/environments/common/compute/main.tf
@@ -29,18 +29,18 @@ data "yandex_vpc_subnet" "subnetwork" {
   name      = var.subnetwork_name_main_folder
 }
 
-# # sudo mkdir /mnt/$FS_NAME && sudo mount -t virtiofs $FS_NAME /mnt/$FS_NAME
-# data "yandex_compute_filesystem" "fs_hrl" {
-#   count = var.filesystem_name != "" ? 1 : 0
-#   name  = "hrl-${var.filesystem_name}"
-# }
-
-# файловое хранилище из другой директории
+# sudo mkdir /mnt/$FS_NAME && sudo mount -t virtiofs $FS_NAME /mnt/$FS_NAME
 data "yandex_compute_filesystem" "fs_hrl" {
-  count     = var.filesystem_name != "" ? 1 : 0
-  folder_id = var.folder_id_main_folder
-  name      = var.filesystem_name_main_folder
+  count = var.filesystem_name != "" ? 1 : 0
+  name  = "hrl-${var.filesystem_name}"
 }
+
+# # файловое хранилище из другой директории
+# data "yandex_compute_filesystem" "fs_hrl" {
+#   count     = var.filesystem_name != "" ? 1 : 0
+#   folder_id = var.folder_id_main_folder
+#   name      = var.filesystem_name_main_folder
+# }
 
 # module "vm-open-vpn" {
 #   source = "../../../modules/vm"
