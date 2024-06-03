9feb9866870fc88b290a422397dda501caef0455
evgenius2049 <evgenius2049@tutanota.com>
2024-06-03 22:48:35 +0300

    теги на роли ansible

--- ansible/init.sh
+++ ansible/init.sh
@@ -10,5 +10,3 @@ cp -u -p $scriptpath/envs/default/env_ansible_test.env.default $scriptpath/envs/
 cp -u -p $scriptpath/envs/default/env_ansible_staging.env.default $scriptpath/envs/prod/env_ansible_staging.env
 cp -u -p $scriptpath/envs/default/env_ansible_onprem.env.default $scriptpath/envs/prod/env_ansible_onprem.env
 cp -u -p $scriptpath/envs/default/env_ansible_prod.env.default $scriptpath/envs/prod/env_ansible_prod.env
-
-cp -u -p $scriptpath/configs/cloud-config.default $scriptpath/configs/cloud-config
