d8f3e08c720b68d446bbc7ebb21cddddd6f237ea
Evgenii Hodkevich <ehodkevich@hr-link.ru>
2024-06-27 15:48:29 +0300

    ref

--- terraform/environments/common/compute/main.tf
+++ terraform/environments/common/compute/main.tf
@@ -47,9 +47,10 @@ locals {
   disk_name_mask             = local.parsed_environments_config["common"]["disk-name-mask"]
   inventory_result_path      = local.parsed_environments_config["common"]["vms-hosts-inventory-result-path"]
 
-  #   parsed_servers_and_disks = jsondecode(data.local_file.servers_and_disks.content)
+  parsed_servers_and_disks = jsondecode(data.local_file.servers_and_disks.content)
   #   servers_and_disks_hrl    = local.parsed_servers_and_disks["hrl"]["common"]
   #   servers_and_disks_strl   = local.parsed_servers_and_disks["strl"]["common"]
+  servers_and_disks_space = local.parsed_servers_and_disks["space"]["common"]
 }
 
 # подсеть из другой директории
@@ -65,66 +66,64 @@ data "yandex_compute_filesystem" "fs_hrl" {
   name  = "hrl-${local.filesystem_name_mask}"
 }
 
-module "vm-open-vpn" {
-  source = "../../../modules/yandex/vm"
+# module "vm-open-vpn" {
+#   source = "../../../modules/yandex/vm"
 
-  # TODO space?
-  name        = "open-vpn"
-  hostname    = "open-vpn"
-  description = "Open VPN"
-  preemptible = var.preemptible
-  nat         = true
+#   # TODO space?
+#   name        = "open-vpn"
+#   hostname    = "open-vpn"
+#   description = "Open VPN"
+#   preemptible = var.preemptible
+#   nat         = true
 
-  cpu                = var.cpu
-  ram                = var.ram
-  boot_disk_image_id = local.boot_disk_image_id
+#   cpu                = var.cpu
+#   ram                = var.ram
+#   boot_disk_image_id = local.boot_disk_image_id
 
-  boot_disk_size    = var.boot_disk_size
-  cloud_config_path = file(var.cloud_config_file_path)
+#   boot_disk_size    = var.boot_disk_size
+#   cloud_config_path = file(var.cloud_config_file_path)
 
-  subnetwork_id          = data.yandex_vpc_subnet.subnetwork.id
-  filesystem_id          = local.filesystem_name_mask != "" ? data.yandex_compute_filesystem.fs_hrl[0].id : ""
-  filesystem_device_name = local.filesystem_name_mask != "" ? "hrl-${local.filesystem_device_name_mask}" : ""
-}
+#   subnetwork_id          = data.yandex_vpc_subnet.subnetwork.id
+#   filesystem_id          = local.filesystem_name_mask != "" ? data.yandex_compute_filesystem.fs_hrl[0].id : ""
+#   filesystem_device_name = local.filesystem_name_mask != "" ? "hrl-${local.filesystem_device_name_mask}" : ""
+# }
 
-module "vm-reverse-nginx" {
-  source = "../../../modules/yandex/vm"
+# module "vm-reverse-nginx" {
+#   source = "../../../modules/yandex/vm"
 
-  # TODO space?
-  name        = "reverse-nginx"
-  hostname    = "reverse-nginx"
-  description = "Реверс nginx"
-  preemptible = var.preemptible
-  nat         = false
+#   # TODO space?
+#   name        = "reverse-nginx"
+#   hostname    = "reverse-nginx"
+#   description = "Реверс nginx"
+#   preemptible = var.preemptible
+#   nat         = false
 
-  cpu                = var.cpu
-  ram                = var.ram
-  boot_disk_image_id = local.boot_disk_image_id
+#   cpu                = var.cpu
+#   ram                = var.ram
+#   boot_disk_image_id = local.boot_disk_image_id
 
-  boot_disk_size    = var.boot_disk_size
-  cloud_config_path = file(var.cloud_config_file_path)
+#   boot_disk_size    = var.boot_disk_size
+#   cloud_config_path = file(var.cloud_config_file_path)
 
-  subnetwork_id          = data.yandex_vpc_subnet.subnetwork.id
-  filesystem_id          = local.filesystem_name_mask != "" ? data.yandex_compute_filesystem.fs_hrl[0].id : ""
-  filesystem_device_name = local.filesystem_name_mask != "" ? "hrl-${local.filesystem_device_name_mask}" : ""
-}
+#   subnetwork_id          = data.yandex_vpc_subnet.subnetwork.id
+#   filesystem_id          = local.filesystem_name_mask != "" ? data.yandex_compute_filesystem.fs_hrl[0].id : ""
+#   filesystem_device_name = local.filesystem_name_mask != "" ? "hrl-${local.filesystem_device_name_mask}" : ""
+# }
 
 # вывод в файл полученных hostname и ip vm-ок
 resource "local_file" "vm_ips" {
 
   content = templatefile("${path.module}/inventory.tpl", {
-    vm_hostnames = flatten(
-      [
-        # module.vm-open-vpn.*.hostname,
-        module.vm-reverse-nginx.*.hostname
-      ]
+    vm_hostnames = concat(
+      [for instance in module.vm-open-vpn : instance.hostname],
+      [for instance in module.vm-reverse-nginx : instance.hostname],
+      [for instance in module.vm-gitlab-runner : instance.hostname],
     )
 
-    vm_ips = flatten(
-      [
-        # module.vm-open-vpn.*.internal_ip,
-        module.vm-reverse-nginx.*.internal_ip
-      ]
+    vm_ips = concat(
+      [for instance in module.vm-open-vpn : instance.internal_ip],
+      [for instance in module.vm-reverse-nginx : instance.internal_ip],
+      [for instance in module.vm-gitlab-runner : instance.internal_ip],
     )
     }
   )
