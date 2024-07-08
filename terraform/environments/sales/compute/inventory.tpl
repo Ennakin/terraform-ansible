3a4660eef42c9775c02d63ef6e00b3b0e6ec4edc
Evgenii Hodkevich <ehodkevich@hr-link.ru>
2024-07-08 14:55:26 +0300

    добавление sales окружения && strl-sales-1

---
+++ terraform/environments/sales/compute/inventory.tpl
@@ -0,0 +1,4 @@
+[sales]
+%{ for index, ip in vm_ips ~}
+${vm_hostnames[index]} ansible_host=${ip}
+%{ endfor ~}
---
+++ terraform/environments/sales/compute/variables.tf
@@ -0,0 +1,41 @@
+variable "main_config" {
+  description = "PATH to main config file"
+  default     = "../../../conf/infra_main_conf.json"
+}
+
+variable "environments_config" {
+  description = "PATH to environments config file"
+  default     = "../../../conf/infra_environments_conf.json"
+}
+
+variable "servers_and_disks" {
+  description = "PATH to servers and disks description file"
+  default     = "../../../conf/infra_servers_and_disks.json"
+}
+
+variable "YC_IAM_TOKEN" {
+  description = "Access token for cloud"
+}
+
+variable "CLOUD_CONFIG" {
+  description = "PATH to cloud-config file"
+}
+
+variable "cpu" {
+  default = 6
+}
+variable "ram" {
+  default = 18
+}
+
+variable "preemptible" {
+  default = true
+}
+
+variable "nat" {
+  default = false
+}
+
+variable "boot_disk_size" {
+  default = 30
+}
