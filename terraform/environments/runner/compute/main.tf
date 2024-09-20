4007a818a2db4bb2aa7dcdb873a1fa12758499b4
Evgenii Hodkevich <ehodkevich@hr-link.ru>
2024-09-20 12:39:47 +0300

    больше cpu и ram для space-gitlab-runner-docker-2

--- terraform/environments/runner/compute/main.tf
+++ terraform/environments/runner/compute/main.tf
@@ -118,8 +118,8 @@ module "vm-gitlab-runner-space-middle-docker" {
   preemptible = false
   nat         = var.nat
 
-  cpu                = contains(["gitlab-runner-docker-1"], each.key) ? 16 : 4
-  ram                = contains(["gitlab-runner-docker-1"], each.key) ? 16 : 12
+  cpu                = contains(["gitlab-runner-docker-1"], each.key) ? 16 : 6
+  ram                = contains(["gitlab-runner-docker-1"], each.key) ? 16 : 18
   boot_disk_image_id = local.boot_disk_image_id
   boot_disk_size     = var.boot_disk_size
   cloud_config_path  = file(var.CLOUD_CONFIG)
